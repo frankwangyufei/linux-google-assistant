8cf861c48aeb2fbe127fe24ab73b6c2ec8940851
frankwangyufei <frankwangyufei559@gmail.com>
2018-12-25 14:13:31 +0800

    initial commit

---
+++ google-assistant.sh
@@ -0,0 +1,11 @@
+for pid in $(pidof -x googlesamples-assistant-pushtotalk); do
+    if [ $pid != $$ ]; then
+        kill -9 $pid
+    fi 
+done
+source ~/env/bin/activate
+googlesamples-assistant-pushtotalk --once 2>&1 | parallel -uj1 '
+if  (echo {}) | grep -e '[rR]ecording' ;
+then
+ beep
+fi '
